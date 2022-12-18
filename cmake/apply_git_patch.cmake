886c5e3a1b1dd6076927a22899996a9c31d49905
Brad Richardson <bradrichardsonmail@gmail.com>
2022-12-17 22:29:46 -0500

    Apply path ignore whitespace

--- cmake/apply_git_patch.cmake
+++ cmake/apply_git_patch.cmake
@@ -3,14 +3,14 @@
 # Arguments are path to git repository and path to the git patch
 #
 macro(apply_git_patch REPO_PATH PATCH_PATH)
-    execute_process(COMMAND git apply --check ${PATCH_PATH}
+    execute_process(COMMAND sh git apply -v --ignore-whitespace --check ${PATCH_PATH}
         WORKING_DIRECTORY ${REPO_PATH}
         RESULT_VARIABLE SUCCESS
         COMMAND_ECHO STDOUT)
 
     if(${SUCCESS} EQUAL 0)
         message("Applying git patch ${PATCH_PATH} in ${REPO_PATH} repository")
-        execute_process(COMMAND git apply ${PATCH_PATH}
+        execute_process(COMMAND sh git apply -v --ignore-whitespace ${PATCH_PATH}
             WORKING_DIRECTORY ${REPO_PATH}
             RESULT_VARIABLE SUCCESS
             COMMAND_ECHO STDOUT)
