605f8e0a1815cdf054baa2f3bfc4edb5c0e84130
Brad Richardson <bradrichardsonmail@gmail.com>
2022-12-22 16:40:15 -0500

    Use correct include directory

--- cmake/ffmpeg_cbs.cmake
+++ cmake/ffmpeg_cbs.cmake
@@ -64,6 +64,7 @@ configure_file(${AVCODEC_GENERATED_SRC_PATH}/packet.h ${CBS_INCLUDE_PATH}/packet
 configure_file(${AVCODEC_GENERATED_SRC_PATH}/sei.h ${CBS_INCLUDE_PATH}/sei.h COPYONLY)
 configure_file(${AVCODEC_GENERATED_SRC_PATH}/vlc.h ${CBS_INCLUDE_PATH}/vlc.h COPYONLY)
 configure_file(${FFMPEG_GENERATED_SRC_PATH}/config.h ${CBS_INCLUDE_PATH}/config.h COPYONLY)
+configure_file(${FFMPEG_GENERATED_SRC_PATH}/libavutil/attributes.h ${CMAKE_BINARY_DIR}/include/libavutil/attributes.h COPYONLY)
 configure_file(${FFMPEG_GENERATED_SRC_PATH}/libavutil/intmath.h ${CMAKE_BINARY_DIR}/include/libavutil/intmath.h COPYONLY)
 
 if (CMAKE_SYSTEM_PROCESSOR MATCHES "(aarch64)|(arm64)")
@@ -129,7 +130,7 @@ set(CBS_SOURCE_FILES
 )
 
 include_directories(
-    ${CBS_INCLUDE_PATH}
+    ${CMAKE_BINARY_DIR}/include
     ${FFMPEG_GENERATED_SRC_PATH}
 )
 
