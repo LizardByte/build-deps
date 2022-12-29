fe43e5cc76df1ed206ca63ecfa3b65df45c8ef8a
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2022-12-29 15:26:33 -0500

    fix cmake-lint errors (#65)

--- cmake/apply_git_patch.cmake
+++ cmake/apply_git_patch.cmake
@@ -1,23 +1,23 @@
-#
 # This macro applies patch to git repository if patch is applicable
 # Arguments are path to git repository and path to the git patch
-#
-macro(apply_git_patch REPO_PATH PATCH_PATH)
-    execute_process(COMMAND git apply -v --ignore-whitespace --check ${PATCH_PATH}
-        WORKING_DIRECTORY ${REPO_PATH}
-        RESULT_VARIABLE SUCCESS
-        COMMAND_ECHO STDOUT)
 
-    if(${SUCCESS} EQUAL 0)
-        message("Applying git patch ${PATCH_PATH} in ${REPO_PATH} repository")
-        execute_process(COMMAND git apply -v --ignore-whitespace ${PATCH_PATH}
-            WORKING_DIRECTORY ${REPO_PATH}
+# APPLY_GIT_PATCH: args = `repo_path`, `patch_path`
+macro(APPLY_GIT_PATCH repo_path patch_path)
+    execute_process(COMMAND git apply -v --ignore-whitespace --check ${patch_path}
+            WORKING_DIRECTORY ${repo_path}
             RESULT_VARIABLE SUCCESS
             COMMAND_ECHO STDOUT)
 
+    if(${SUCCESS} EQUAL 0)
+        message("Applying git patch ${patch_path} in ${repo_path} repository")
+        execute_process(COMMAND git apply -v --ignore-whitespace ${patch_path}
+                WORKING_DIRECTORY ${repo_path}
+                RESULT_VARIABLE SUCCESS
+                COMMAND_ECHO STDOUT)
+
         if(${SUCCESS} EQUAL 1)
             # We don't stop here because it can happen in case of parallel builds
-            message(WARNING "\nError: failed to apply the patch patch: ${PATCH_PATH}\n")
+            message(WARNING "\nError: failed to apply the patch patch: ${patch_path}\n")
         endif()
     endif()
 endmacro()
