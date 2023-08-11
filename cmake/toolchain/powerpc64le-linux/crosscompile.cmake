ede506b43314d5159e559d149a82cce80440c76a
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2023-08-10 20:26:02 -0400

    build(ffmpeg): add powerpc64le architecture (#180)

---
+++ cmake/toolchain/powerpc64le-linux/crosscompile.cmake
@@ -0,0 +1,10 @@
+# CMake toolchain file for cross compiling to linux powerpc64le
+
+set(CROSS_COMPILE_PPC 1)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ppc64le)
+
+set(CMAKE_C_COMPILER powerpc64le-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER powerpc64le-linux-gnu-g++)
+
+SET(CMAKE_FIND_ROOT_PATH  /usr/powerpc64le-linux-gnu)
