3b85091635f7e963dd19e22d769125bb71145dda
Coia Prant <coiaprant@gmail.com>
2025-05-12 21:53:51 +0800

    fix(patch): fix apply patches for MF
    Wrong reference, maybe you forgot to change it when you copied it?
    
    Signed-off-by: Coia Prant <coiaprant@gmail.com>

--- cmake/ffmpeg/mf.cmake
+++ cmake/ffmpeg/mf.cmake
@@ -1,7 +1,7 @@
 if(BUILD_FFMPEG_ALL_PATCHES OR BUILD_FFMPEG_MF_PATCHES)
     file(GLOB FFMPEG_MF_FILES ${CMAKE_CURRENT_SOURCE_DIR}/patches/FFmpeg/MF/*.patch)
 
-    foreach(patch_file ${FFMPEG_VAAPI_FILES})
+    foreach(patch_file ${FFMPEG_MF_FILES})
         APPLY_GIT_PATCH(${FFMPEG_GENERATED_SRC_PATH} ${patch_file})
     endforeach()
 endif()
