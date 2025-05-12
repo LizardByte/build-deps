4de090ba02684c166c1e7ee3668a695b38bb642a
Coia Prant <coiaprant@gmail.com>
2025-05-12 22:02:04 +0800

    fix(patch): fix apply patches for x265
    Wrong reference, maybe you forgot to change it when you copied it?
    
    Signed-off-by: Coia Prant <coiaprant@gmail.com>

--- cmake/ffmpeg/x265.cmake
+++ cmake/ffmpeg/x265.cmake
@@ -1,7 +1,7 @@
 if(BUILD_FFMPEG_ALL_PATCHES OR BUILD_FFMPEG_X265_PATCHES)
     file(GLOB FFMPEG_X265_FILES ${CMAKE_CURRENT_SOURCE_DIR}/patches/FFmpeg/x265/*.patch)
 
-    foreach(patch_file ${FFMPEG_SVT_X265_FILES})
+    foreach(patch_file ${FFMPEG_X265_FILES})
         APPLY_GIT_PATCH(${FFMPEG_GENERATED_SRC_PATH} ${patch_file})
     endforeach()
 endif()
