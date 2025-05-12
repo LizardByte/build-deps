56553614c36ed454b7f3618b4108e7200406cec5
Coia Prant <coiaprant@gmail.com>
2025-05-13 01:10:54 +0800

    fix(patch): fix apply patches (#425)

--- cmake/ffmpeg/x264.cmake
+++ cmake/ffmpeg/x264.cmake
@@ -1,7 +1,7 @@
 if(BUILD_FFMPEG_ALL_PATCHES OR BUILD_FFMPEG_X264_PATCHES)
     file(GLOB FFMPEG_X264_FILES ${CMAKE_CURRENT_SOURCE_DIR}/patches/FFmpeg/x264/*.patch)
 
-    foreach(patch_file ${FFMPEG_SVT_X264_FILES})
+    foreach(patch_file ${FFMPEG_X264_FILES})
         APPLY_GIT_PATCH(${FFMPEG_GENERATED_SRC_PATH} ${patch_file})
     endforeach()
 endif()
