9cf8b0fd1f9d78c3a94a4bf631a766aedf66fadf
David Lane <42013603+ReenigneArcher@users.noreply.github.com>
2026-04-10 08:54:54 -0400

    chore: update CPM pacakge-lock file for renovate (#646)

--- package-lock.cmake
+++ package-lock.cmake
@@ -6,25 +6,32 @@
 # Ideally, both are the same, which might not always be possible: https://github.com/cpm-cmake/CPM.cmake/issues/603
 # This is needed to support CPM_USE_LOCAL_PACKAGES
 
-# TODO: update dependencies with renovate
-# https://joht.github.io/johtizen/automation/2022/08/03/keep-your-cpp-dependencies-up-to-date.html
+# Renovate-bot will update the versions and hashes in this file when a new version of a dependency is released.
+# The comments above each dependency are used by renovate to identify the dependencies and extract the version numbers.
+# See https://github.com/LizardByte/.github/blob/master/renovate-config.json5 for the configuration of renovate.
 
 set(PATCH_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/patches")
 
 # Boost
+# renovate: datasource=github-release-attachments depName=boostorg/boost extractVersion=^boost-(?<version>.+)$
+set(BOOST_VERSION 1.89.0)
+set(BOOST_SHA256 67acec02d0d118b5de9eb441f5fb707b3a1cdd884be00ca24b9a73c995511f74)
+set(BOOST_TAG boost-${BOOST_VERSION})
 CPMDeclarePackage(Boost
         NAME Boost
-        VERSION 1.89.0
-        URL https://github.com/boostorg/boost/releases/download/boost-1.89.0/boost-1.89.0-cmake.tar.xz
-        URL_HASH SHA256=67acec02d0d118b5de9eb441f5fb707b3a1cdd884be00ca24b9a73c995511f74
+        VERSION ${BOOST_VERSION}
+        URL https://github.com/boostorg/boost/releases/download/${BOOST_TAG}/${BOOST_TAG}-cmake.tar.xz
+        URL_HASH SHA256=${BOOST_SHA256}
         DOWNLOAD_ONLY YES
 )
 
 # libva
+# renovate: datasource=github-tags depName=intel/libva
+set(LIBVA_VERSION 2.23.0)
 CPMDeclarePackage(libva
         NAME libva
-        VERSION 2.23.0
+        VERSION ${LIBVA_VERSION}
         GIT_REPOSITORY https://github.com/intel/libva.git
-        GIT_TAG 2.23.0
+        GIT_TAG ${LIBVA_VERSION}
         DOWNLOAD_ONLY YES
 )
